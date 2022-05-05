d76615cbb7e8bfd4ebafb3743b6e8a5316cc8e9a
Monalisa Menezes <psimonafmenezes@gmail.com>
2022-05-04 23:43:13 -0300

    feat: create card

---
+++ src/components/DCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="w-72 h-56 bg-orange rounded-md p-4 flex flex-col justify-between items-center"
+  >
+    <h2 class="text-black font-bold text-lg">{{ coinInfo.name }}</h2>
+    <img :src="coinInfo.url" />
+    <p class="text-black"><strong>Date:</strong> {{ coinInfo.date}}</p>
+    <h3 class="text-black">R$ {{ coinInfo.price.toFixed(2).replace('.', ',') }} BRL</h3>
+  </div>
+</template>
+
+<script>
+import DButton from "./DButton.vue";
+export default {
+  components: { DButton },
+  props: {
+    coinInfo: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Date.vue
+++ src/views/Date.vue
@@ -1,24 +1,37 @@
 <template>
   <div>
     <d-header />
-    <section class="flex-col items-center">
+    <section class="flex flex-col items-center">
       <h2 class="text-2xl text-center mt-16 mb-10 font-light">Insert Date</h2>
 
-      <form action="" class="flex flex-col items-center mt-16">
-        <h6 class="mb-5 text-lg font-bold">Bitcoin</h6>
-        <input class="mb-5" type="date" />
-        <input class="mb-5" type="time" />
+      <form
+        class="flex flex-col items-center mt-16"
+        v-if="requestCoin === false"
+      >
+        <h6 class="mb-5 text-lg font-bold">{{}}</h6>
+        <input class="mb-5" type="date" v-model="info.date" />
+        <input class="mb-5" type="time" v-model="info.time" />
         <div
           class="w-full mt-10 flex sm:flex-col lg:flex-row sm:items-center justify-center"
         >
+          <d-button
+            class="sm:mb-7 lg:mb-0 lg:mr-14"
+            @click.prevent="requestAPi()"
+            >search</d-button
+          >
           <router-link to="/">
-            <d-button class="sm:mb-7 lg:mb-0 lg:mr-14" 
-              >home</d-button
-            >
+            <d-button color="bg-green">home</d-button>
           </router-link>
-          <d-button color="bg-green">search</d-button>
         </div>
       </form>
+      <d-card :coinInfo="info" v-if="requestCoin === true" />
+      <d-button
+        v-if="requestCoin === true"
+        class="mt-4 w-72"
+        @click="requestCoin = !requestCoin"
+        color="bg-green"
+        >new request</d-button
+      >
     </section>
     <d-footer class="mt-32" />
   </div>
@@ -28,8 +41,35 @@
 import DButton from "../components/DButton.vue";
 import DFooter from "../components/DFooter.vue";
 import DHeader from "../components/DHeader.vue";
+import DCard from "../components/DCard.vue";
 export default {
-  components: { DHeader, DButton, DFooter },
+  components: { DHeader, DButton, DFooter, DCard },
+  data() {
+    return {
+      requestCoin: false,
+      info: { name: "Luna", url: "", date: "", price: 0, time: "" },
+    };
+  },
+  methods: {
+    requestAPi() {
+
+      
+      console.log(this.info.date)
+      const [year, mounth, day] = this.info.date.split("-");
+      const [hours, minutes] = this.info.time.split(":");
+      const dateRequest = new Date(+year, mounth - 1, +day, +hours, +minutes);
+      const unixTimestamp = Math.floor(dateRequest.getTime() / 1000);
+      const dateAtt = new Date();
+      console.log(dateRequest);
+      console.log(dateAtt)
+
+      const unixDateAtt = Math.floor(dateAtt.getTime() / 1000);
+
+      console.log(unixDateAtt);
+
+      console.log(unixTimestamp);
+    },
+  },
 };
 </script>
 
